d965cefb9784aff54386240145fa0a5cbf5b8d33
Allan Saraiva <allan.saraiva2001@gmail.com>
2021-06-27 20:22:16 -0300

    update

---
+++ seção 2 python básico/aula019.py
@@ -0,0 +1,7 @@
+nome = 'Allan'
+idade = 32
+altura = 1.80
+peso = 80
+imc = peso / altura ** 2
+print(f'{nome} tem {idade} anos de idade e seu imc é {imc:.0f} ')
+print('{} tem {} anos de idade e seu imc é {:.2f}'.format(nome, idade, imc))
