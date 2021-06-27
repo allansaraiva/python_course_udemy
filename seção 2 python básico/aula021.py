d856deb21c0c3a35c50260ae3069aff8f1bfe299
Allan Saraiva <allan.saraiva2001@gmail.com>
2021-06-27 20:38:28 -0300

    first commit

---
+++ seção 2 python básico/aula021.py
@@ -0,0 +1,11 @@
+# Resolução da aula anterior (aula020)
+nome = 'Allan'
+idade = 20
+peso = 80
+altura = 1.84
+ano_atual = 2021
+nascimento = ano_atual - idade
+imc = peso / (altura ** 2)
+print(f'{nome} tem {idade} anos, {altura} de altura e pesa {peso}Kg.')
+print(f'O imc de {nome} é {imc:.2f}.')
+print(f'{nome} nasceu em {nascimento}')
