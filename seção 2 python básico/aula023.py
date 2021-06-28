457cbd794eb96e82183ed474e5b89fbbb2d4091c
Allan Saraiva <allan.saraiva2001@gmail.com>
2021-06-27 21:30:30 -0300

    first commit

---
+++ seção 2 python básico/aula023.py
@@ -0,0 +1,10 @@
+"""
+condições IF, ELIF e ELSE.
+"""
+
+n1 = int(input('digite um número: '))
+n2 = int(input('digite outro número: '))
+if n1 == n2:
+    print('são iguais.')
+else:
+    print('Não são iguais.')
