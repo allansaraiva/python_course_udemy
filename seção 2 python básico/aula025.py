2d7c26f0c6f133c6f4b3ba723b9022ed98f73167
Allan Saraiva <allan.saraiva2001@gmail.com>
2021-06-30 01:05:54 -0300

    first commit

---
+++ seção 2 python básico/aula025.py
@@ -0,0 +1,29 @@
+"""
+Operadores lógicos
+and, or, not
+in, not in
+"""
+# nome = str(input('Seu nome: '))
+# f nome == 'allan' or nome == 'roger':
+#    print('True')
+# else:
+#     print('false')
+
+
+# nome = str(input('Seu nome: '))
+# if not nome:  # Se a variável estiver vazia, o programa imprime uma mensagem de erro.
+#     print('por favor, nos informe o seu nome.')
+
+
+# nome = str(input('Seu nome: '))
+# if 'l' in nome:
+#     print('Seu nome tem a letra L.')
+# else:
+#     print('Seu nome não tema letra L.')
+
+
+# nome = str(input('Seu nome: '))
+# if 'l' not in nome:
+#     print('Não tem L no meu nome.')
+# else:
+#     print('Tem l no seu nome.')
