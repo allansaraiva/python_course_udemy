d8342da7b5d40706335b765774b18a4ba84bbb4b
Allan Saraiva <allan.saraiva2001@gmail.com>
2021-06-30 01:35:27 -0300

    first commit

---
+++ seção 2 python básico/aula026.py
@@ -0,0 +1,17 @@
+"""
+len
+"""
+# senha = input('Sua senha: ')
+# qtd_senha = len(senha)
+# print(qtd_senha)
+
+# if qtd_senha < 8:
+#     print('Senha muito fraca.')
+# else:
+#     print('Senha forte.')
+
+
+string_1 = input('Digite alguma coisa: ')
+string_2 = input('Digite outra coisa: ')
+
+print(f'Foram digitados um total de {len(string_1) + len(string_2)} caracteres')
