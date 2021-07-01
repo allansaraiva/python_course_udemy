27ed32ee4de0ff8f9764f3d86abdbf41d645ce9c
Allan Saraiva <allan.saraiva2001@gmail.com>
2021-07-01 01:02:49 -0300

    first commit

---
+++ seção 2 python básico/aula032.py
@@ -0,0 +1,13 @@
+"""
+Manipulando strings
+
+https://docs.python.org/3/library/stdtypes.html - tipos
+https://docs.python.org/3/library/functions.html - funções
+"""
+
+# texto = 'python'
+# print(texto[0])  # imprime apenas a letra P.
+
+
+texto = 'python'
+print(texto[2:6])  # imprime a sigla THON.
