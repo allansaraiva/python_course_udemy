228170c11bfb21713202b632fb33ddc47effeb54
Allan Saraiva <allan.saraiva2001@gmail.com>
2021-07-01 02:26:49 -0300

    first commit

---
+++ seção 2 python básico/aula034.py
@@ -0,0 +1,12 @@
+"""
+While/Else
+"""
+
+contador = 1
+acumulador = 1
+while contador <= 10:
+    print(contador, acumulador)
+    acumulador = acumulador + contador
+    contador += 1
+else:
+    print('Fim!')
