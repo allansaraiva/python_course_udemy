852d245bb3dfb921a1f7adf983f88097070cf6c8
Allan Saraiva <allan.saraiva2001@gmail.com>
2021-07-05 01:44:52 -0300

    first commit

---
+++ seção 3 python intermediário/aula052.py
@@ -0,0 +1,24 @@
+"""
+Funções (def) em Python - Parte 2
+"""
+
+
+# def division(n1, n2):
+#     if n2 == 0:
+#         return
+#
+#     return n1 / n2
+#
+# divide = division(8, 0)
+#
+# if divide:
+#     print(divide)
+# else:
+#     print('Conta inválida')
+
+
+def dumb():
+    return 1
+
+var = dumb()
+print(var, type(var))
